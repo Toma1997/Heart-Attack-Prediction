a6cbb60bd25a9ac103bdfc2b62dd50e9e9fc02ae
Toma1997 <toma.joksimovic@gmail.com>
2020-07-13 02:09:54 +0200

    Data cleaning and edited docs

--- heart_attack_prediction.r
+++ heart_attack_prediction.r
@@ -12,7 +12,45 @@ head(data, 10)
 ```
 
 ```{r}
+colSums(data, na.rm = TRUE, dims = 1) # Sum by columns without NA values.
+```
+```{r}
+colSums(is.na(data)) # There are no missig values.
+```
+```{r}
+# If there are some missing values, replace it with mean value.
+data$thalach[is.na(data$thalach)] <- mean(data$thalach, na.rm = TRUE)
+na.omit(data) # Exclude missing values.
+```
+
+
+```{r}
 str(data) # List the structure of the data.
 ```
 
+```{r}
+par(mfrow = c(2, 2)) 
+hist(data$age, main = "Age Distribution", xlab = "age")
+hist(data$thalach, main = "Max Heart Rate Distribution", xlab = "thalach")
+hist(data$chol, main = "Serum cholestoral ", xlab = "chol")
+hist(data$restbps, main = "Resting blood pressure", xlab = "restbps")
+#hist(data$age, main = "Pateints Age Distribution", xlab = "age")
+```
+
+```{r}
+x <- data$age
+y <- data$thalach 
+plot(x, y, main = "Relationship between age and maximum heart rate achieved", xlab = "age", ylab = "Max heart rate achieved", pch = 15, frame = FALSE)
+abline(lm(y ~ x, data = data), col = "blue") # Correlation is weak.
+
+```
+```{r}
+summary(data) # Statistic informations
+```
+
+```{r}
+# Drop target column to make new prediction on dataset.
+x = subset(data, select = -c(target))
+y = data$target
+```
 
